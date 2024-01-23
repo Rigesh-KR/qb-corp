caf3926a4717bcf6c6f0262ea5f2664d230755f4
Gokul <gokulks.stratagile@gmail.com>
2024-01-23 19:29:37 +0530

    tab

--- assets/js/script.js
+++ assets/js/script.js
@@ -254,3 +254,43 @@ $(".qbc-cat-links__outer--slick").slick({
     // instead of a settings object
   ],
 });
+
+//johnsonville product tab
+
+$('.qbc-jhnvil-slide').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: $('.qbc-product-tab__arrowl'),
+  nextArrow: $('.qbc-product-tab__arrowr'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
