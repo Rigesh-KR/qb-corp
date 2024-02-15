b96600439b9709ab58ce249530f44df808c9f385
Sanjay <sanjay.e@strat-agile.com>
2024-02-15 18:14:53 +0530

    tab sec

--- assets/js/script.js
+++ assets/js/script.js
@@ -334,3 +334,20 @@ document.addEventListener("DOMContentLoaded", function () {
     updateScrollButtons();
   }
 });
+
+// tab active
+$('.nav-tabs li a').click(function (e) {
+  //get selected href
+  var href = $(this).attr('href');
+
+  //set all nav tabs to inactive
+  $('.nav-tabs li').removeClass('active');
+  $('.nav-link').removeClass('active');
+
+  //get all nav tabs matching the href and set to active
+  $('.nav-tabs li a[href="' + href + '"]').closest('li').addClass('active');
+
+  //active tab
+  $('.tab-pane').removeClass('active');
+  $('.tab-pane' + href).addClass('active');
+})
